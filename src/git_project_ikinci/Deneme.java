6f44b530d6625c334b37a258034be8467a92d6e8
ayhanakoz <ayhanakoz79@gmail.com>
2020-08-10 00:35:42 +0300

    ekle

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -5,7 +5,8 @@ public class Deneme {
 	public static void main(String[] args) {
 		
     System.out.println("Herkese Selamlar");
-    
+    System.out.println("Nasilsiniz");
+    System.out.println("Conflict-Ayhan");
 	}
 
 }
